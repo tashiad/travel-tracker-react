3b3f75d94d69b886719a7384235a659d6e388f39
tashiad <tashiadavis10@gmail.com>
2021-03-13 14:42:28 -0700

    Render all components with default content

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import './App.css'
 import Header from '../Header/Header'
+import Form from '../Form/Form'
+import Stats from '../Stats/Stats'
+import Trips from '../Trips/Trips'
 
 class App extends Component {
   constructor() {
@@ -18,6 +21,14 @@ class App extends Component {
     return (
       <>
         <Header name={this.state.user.name} />
+        <main>
+          <aside className="sidebar">
+            <img className="icon" src="../../images/undraw_travel_mode_7sf4.svg" alt="woman traveling with a suitcase" />
+            <Form />
+            <Stats />
+          </aside>
+          <Trips />
+        </main>
       </>
     )
   }
