5a71aa913e8a2fb2ad30e7efdbfffb93d12922b0
tashiad <tashiadavis10@gmail.com>
2021-03-13 15:55:50 -0700

    Format flight and lodging cost

--- src/components/Trip/Trip.js
+++ src/components/Trip/Trip.js
@@ -2,6 +2,11 @@ import React from 'react'
 import './Trip.css'
 
 const Trip = ({ id, date, duration, travelers, destination, alt, image, flights, lodging }) => {
+  const formatCost = (number) => {
+    const formatted = number.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+    return formatted
+  }
+
   return (
     <article className="card">
       <h3 className="card-destination">{destination}</h3>
@@ -9,8 +14,8 @@ const Trip = ({ id, date, duration, travelers, destination, alt, image, flights,
       <p className="card-travelers">{travelers}</p>
       <p className="card-date">{date}</p>
       <p className="card-duration">{duration}</p>
-      <p className="card-lodging">{lodging}</p>
-      <p className="card-flights">{flights}</p>
+      <p className="card-lodging">${formatCost(lodging)}</p>
+      <p className="card-flights">${formatCost(flights)}</p>
     </article>
   )
 }
