a73d2768b6958fe495777ab2b582428d803449f3
tashiad <tashiadavis10@gmail.com>
2021-03-13 16:15:22 -0700

    Format date on cards

--- src/components/Trip/Trip.js
+++ src/components/Trip/Trip.js
@@ -2,6 +2,12 @@ import React from 'react'
 import './Trip.css'
 
 const Trip = ({ id, date, duration, travelers, destination, alt, image, flights, lodging }) => {
+  const formatDate = () => {
+    const nums = date.split('/')
+    const formatted = `${nums[1]}/${nums[2]}/${nums[0]}`
+    return formatted
+  }
+
   const formatCost = (number) => {
     const formatted = number.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
     return formatted
@@ -19,7 +25,7 @@ const Trip = ({ id, date, duration, travelers, destination, alt, image, flights,
       <h3 className="card-destination">{destination}</h3>
       <img className="card-image" src={image} alt={alt} />
       <p className="card-travelers"><strong>Travelers:</strong> {travelers}</p>
-      <p className="card-date"><strong>Start Date:</strong> {date}</p>
+      <p className="card-date"><strong>Start Date:</strong> {formatDate()}</p>
       <p className="card-duration"><strong>Duration:</strong> {duration} days</p>
       <p className="card-lodging"><strong>Lodging:</strong> ${formatCost(lodging)} per night</p>
       <p className="card-flights"><strong>Flights:</strong> ${formatCost(flights)} per person</p>
